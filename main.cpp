add295cfe0edaecf4942925c726d17742275882c
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-07-23 17:14:32 +0300

    Added serialization for json

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "ToDoList.h"
 
+void saveJsonToFile(const std::string& path, const json& j)
+{
+    std::ostream file(path);
+    if(!file.is_open()){
+        throw std::runtime_error("Failed to open file for writing: "+path);
+    }
+
+    file << j.dump(4);
+}
+
 void main()
 {
     ToDoList list {"resources/list.json"};
